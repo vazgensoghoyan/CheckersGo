0053e71f805092edb568e71dc2fa82d9743446a2
adis <adisavelina@gmail.com>
2025-09-28 00:00:28 +0500

    first fiegures and one func

--- internal/checkers/checkers.go
+++ internal/checkers/checkers.go
@@ -1,1 +1,10 @@
 package checkers
+type Board struct{
+	cells[][]int // Доска, это двумерный массив
+}
+type Fiegure struct{
+	row int		// строка
+	col int		// колонна
+	player int  // игрок 1 или 2
+	isKing bool // Королева или нет(t or f)
+}
