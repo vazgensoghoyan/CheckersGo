ef6cfa22d27b8bfa81a97ba76c951a9e2896d020
vazgensoghoyan <sogvazulchik@gmail.com>
2025-09-27 22:18:58 +0300

    structure of checkers.go file

--- internal/checkers/checkers.go
+++ internal/checkers/checkers.go
@@ -1,10 +1,43 @@
 package checkers
-type Board struct{
-	cells[][]int // Доска, это двумерный массив
+
+type Checkers struct {
+	board [][]Figure
 }
-type Fiegure struct{
-	row int		// строка
-	col int		// колонна
-	player int  // игрок 1 или 2
-	isKing bool // Королева или нет(t or f)
+
+func NewCheckers() *Checkers {
+	board := make([][]Figure, 8)
+
+	for i := range board {
+		board[i] = make([]Figure, 8)
+	}
+
+	// расставляем фигуры
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			board[i][j] = Figure{isNone: true, isWhite: false, isKing: false}
+		}
+	}
+
+	// черные
+	// белые
+
+	return &Checkers{board: board}
+}
+
+func StartGame(checkers *Checkers) {
+	// начинаем обрабатывать действия игроков и менять доску соответствующе
+}
+
+type Figure struct {
+	isNone  bool
+	isWhite bool
+	isKing  bool
+}
+
+// это будет где то извне происходить, так будет игра начинаться
+func main() {
+	var game = NewCheckers()
+
+	StartGame(game)
 }
